d669a4ec4878c03c239930d0a74ad5f3b9ee742e
lakhara <siddharth.lakhara@gmail.com>
2018-01-29 23:07:45 +0530

    Test suit for multiple wins -> combination of strike and spare

--- roll.test.js
+++ roll.test.js
@@ -34,3 +34,21 @@ describe('testing at end: ', () => {
 			toBe(103);
 	});
 });
+
+describe('Multiple wins: ', () => {
+	test('Test for multiple spare: ', () =>
+		expect(roll([3, 6, 4, 6, 4, 6, 4, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).
+			toBe(104));
+
+	test('Test for multiple spare and strike at end: ', () =>
+		expect(roll([3, 6, 4, 6, 4, 6, 4, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 10,6,6])).
+			toBe(117));
+
+	test('Test for spare and multiple strikes: ', () =>
+		expect(roll([4, 6, 10, 10, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).
+			toBe(125));
+
+	test('Test for multiple spares & strikes: ', () =>
+		expect(roll([4, 6, 10, 10, 3, 6, 4, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).
+			toBe(129));
+});
