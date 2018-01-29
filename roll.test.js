674dfacc6790543ceb1c92771d5541d4ea736176
lakhara <siddharth.lakhara@gmail.com>
2018-01-29 22:50:05 +0530

    testing at end: spare and strike

--- roll.test.js
+++ roll.test.js
@@ -22,3 +22,15 @@ describe('Basic cases: ', () => {
 	});
 
 });
+
+describe('testing at end: ', () => {
+	test('Test for spare at the end: ', () => {
+		expect(roll([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 4, 6, 2])).
+			toBe(93);
+	});
+
+	test('Test for strike at the end: ', () => {
+		expect(roll([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 10, 6, 6])).
+			toBe(103);
+	});
+});
